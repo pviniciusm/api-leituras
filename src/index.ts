d7fe6ec0144b7909b49bc582bb27b5972dd0fd4a
pviniciusm <pvinics@gmail.com>
2023-02-22 20:16:36 -0300

    Configuração inicial do servidor express.
    Co-authored-by: Claudinei Braga de Oliveira <claudinei05@users.noreply.github.com>
    Co-authored-by: GabiQuadros <GabiQuadros@users.noreply.github.com>
    Co-authored-by: leonardo1515 <leonardo1515@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Deu certo");
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Ok",
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("API está rodando");
+});
