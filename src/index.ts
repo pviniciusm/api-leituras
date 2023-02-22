e5250364428575f10aa78690ffb58fcd03ccbfa1
pviniciusm <pvinics@gmail.com>
2023-02-22 20:51:53 -0300

    Criação do controller e do router de user

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { userRoutes } from "./routes/user.routes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-    res.send({
-        message: "Ok",
-    });
-});
+app.use("/user", userRoutes());
 
 app.listen(process.env.PORT, () => {
     console.log("API está rodando");
