7713eacf14145712aafc2a7800f515d9ce1199c2
pviniciusm <pvinics@gmail.com>
2023-02-23 22:36:34 -0300

    add getter para meta de livros do usuario

--- src/models/usuario.models.ts
+++ src/models/usuario.models.ts
@@ -2,62 +2,65 @@ import { v4 as creatUuid } from "uuid";
 import { Livro } from "./leitura.models";
 
 export class User {
-  private _id: string;
-  private _livros: Livro[];
+    private _id: string;
+    private _livros: Livro[];
 
-  constructor(
-    private _username: string,
-    private _password: string,
-    private _nome: string,
-    private _meta: number
-  ) {
-    this._id = creatUuid();
-    this._livros = [];
-  }
+    constructor(
+        private _username: string,
+        private _password: string,
+        private _nome: string,
+        private _meta: number
+    ) {
+        this._id = creatUuid();
+        this._livros = [];
+    }
 
-  public get livros() {
-    return this._livros;
-  }
-  public set livros(livros: Livro[]) {
-    this._livros = livros;
-  }
-  public addLivros(livro: Livro) {
-    this.livros.push(livro);
-  }
-  public get id() {
-    return this._id;
-  }
-  public get nome() {
-    return this._nome;
-  }
+    public get livros() {
+        return this._livros;
+    }
+    public set livros(livros: Livro[]) {
+        this._livros = livros;
+    }
+    public addLivros(livro: Livro) {
+        this.livros.push(livro);
+    }
+    public get id() {
+        return this._id;
+    }
+    public get nome() {
+        return this._nome;
+    }
 
-  public get password() {
-    return this._password;
-  }
+    public get password() {
+        return this._password;
+    }
 
-  public set userName(userName: string) {
-    this._username = userName;
-  }
-  public set password(password: string) {
-    this._password = password;
-  }
-  public set nome(nome: string) {
-    this._nome = nome;
-  }
-  public set meta(meta: number) {
-    this._meta = meta;
-  }
-  public get username() {
-    return this._username;
-  }
+    public set userName(userName: string) {
+        this._username = userName;
+    }
+    public set password(password: string) {
+        this._password = password;
+    }
+    public set nome(nome: string) {
+        this._nome = nome;
+    }
+    public set meta(meta: number) {
+        this._meta = meta;
+    }
+    public get meta() {
+        return this._meta;
+    }
+    public get username() {
+        return this._username;
+    }
 
-  public toJson() {
-    return {
-      id: this._id,
-      livros: this._livros,
-      username: this._username,
-      nome: this._nome,
-      meta: this._meta,
-    };
-  }
+    public toJson() {
+        return {
+            id: this._id,
+            livros: this._livros,
+            username: this._username,
+            nome: this._nome,
+            meta: this._meta,
+        };
+    }
 }
